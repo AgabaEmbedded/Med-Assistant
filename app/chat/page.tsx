917071cd544dbabebe75469c34f807ee4b24ee80
Daniel <danielukoha101@gmail.com>
2024-07-20 22:07:44 +0100

    feat: vercel AI sdk with gemini integration

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,26 +1,79 @@
 "use client";
 
-import { useChat } from "ai/react";
+import { CoreMessage } from "ai";
+import { useState } from "react";
+import { continueConversation } from "../actions";
+import { readStreamableValue } from "ai/rsc";
+import Image from "next/image";
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
 
 export default function Chat() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat();
+  const [messages, setMessages] = useState<CoreMessage[]>([]);
+  const [input, setInput] = useState("");
 
   return (
-    <section className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-      {messages.map((message) => (
-        <div key={message.id} className="whitespace-pre-wrap">
-          {message.role === "user" ? "User: " : "AI: "}
-          {message.content}
-        </div>
-      ))}
+    <section className="flex flex-col w-full items-center pb-28">
+      <div className="flex flex-col gap-4 w-full py-4 px-4 max-w-2xl">
+        {messages.map((message, id) => (
+          <div
+            key={id}
+            className="flex w-full max-w-[75%] items-center gap-4 odd:self-start odd:justify-start even:self-end even:justify-end even:mb-4"
+          >
+            {message.role === "user" ? (
+              <>
+                <div className=" px-4 py-3 rounded-lg  bg-[#2563eb] text-white">
+                  <p>{message.content as string}</p>
+                </div>
+              </>
+            ) : (
+              <>
+                <Image
+                  src="/images/chatbot.png"
+                  alt="Assistant logo"
+                  width={30}
+                  height={30}
+                />
+                <div className=" px-4 py-3 rounded-lg bg-[#f1f5f9]">
+                  <p>{message.content as string}</p>
+                </div>
+              </>
+            )}
+          </div>
+        ))}
+      </div>
 
-      <form onSubmit={handleSubmit}>
-        <input
-          className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
-          value={input}
-          placeholder="Say something..."
-          onChange={handleInputChange}
-        />
+      <form
+        className="w-full text-center"
+        onSubmit={async (e) => {
+          e.preventDefault();
+          const newMessages: CoreMessage[] = [
+            ...messages,
+            { content: input, role: "user" },
+          ];
+
+          setMessages(newMessages);
+          setInput("");
+
+          const result = await continueConversation(newMessages);
+
+          for await (const content of readStreamableValue(result)) {
+            setMessages([
+              ...newMessages,
+              { role: "assistant", content: content as string },
+            ]);
+          }
+        }}
+      >
+        <div className="fixed bottom-0 w-full bg-white">
+          <input
+            className="py-4 px-4 mb-4 w-full max-w-xl border border-gray-500 rounded-full outline-none focus:outline"
+            value={input}
+            placeholder="Say something..."
+            onChange={(e) => setInput(e.target.value)}
+          />
+        </div>
       </form>
     </section>
   );
