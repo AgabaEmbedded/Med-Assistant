d16e7713478ffcad7fc3e157afd0067e1bb3b26f
Daniel <danielukoha101@gmail.com>
2024-07-21 12:41:08 +0100

    style: home and chatbot navigation links

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header className="py-4 px-8 flex bg-white backdrop-blur-lg sticky top-0 border-b-1">
+          <nav>
+            <h2 className="text-xl font-bold text-black">
+              <Link href="/">Sigma AI</Link>
+            </h2>
+          </nav>
+        </header>
+        {children}
+        <footer></footer>
+      </body>
     </html>
   );
 }
